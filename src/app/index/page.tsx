63d74c55160abd2eed53a5e71c04af7eb73a4126
ege <egeblackbird@gmail.com>
2026-02-22 00:25:01 +0300

    fix: guard against null user

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,5 +1,7 @@
 import { GlobalIndexPage } from "@/components/pages/global-index-page";
 
+export const dynamic = "force-dynamic";
+
 export default function IndexRoutePage(): JSX.Element {
   return <GlobalIndexPage />;
 }
